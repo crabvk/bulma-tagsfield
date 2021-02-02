39dcb2d5b3e733532a7a629ddaf8e808c855df51
Vyacheslav Konovalov <crabvk@protonmail.com>
2021-02-02 07:17:50 +0500

    Update repo url, generate new docs

--- dist/docs.js
+++ dist/docs.js
@@ -1,111 +1,22 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// define __esModule on exports
-/******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
-/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./docs/index.js");
-/******/ })
-/************************************************************************/
-/******/ ({
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
 
 /***/ "./docs/index.js":
 /*!***********************!*\
   !*** ./docs/index.js ***!
   \***********************/
-/*! no exports provided */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_tagsfield__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/tagsfield */ \"./src/tagsfield.js\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ \"./docs/index.scss\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_index_scss__WEBPACK_IMPORTED_MODULE_1__);\n\n\ndocument.querySelectorAll('.tagsfield').forEach(el => new _src_tagsfield__WEBPACK_IMPORTED_MODULE_0__[\"default\"](el));\n\n//# sourceURL=webpack:///./docs/index.js?");
-
-/***/ }),
-
-/***/ "./docs/index.scss":
-/*!*************************!*\
-  !*** ./docs/index.scss ***!
-  \*************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
-
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./docs/index.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_tagsfield__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/tagsfield */ \"./src/tagsfield.js\");\n/* harmony import */ var _index_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./index.scss */ \"./docs/index.scss\");\ndocument.querySelectorAll(\".tagsfield\").forEach(a=>new _src_tagsfield__WEBPACK_IMPORTED_MODULE_0__.default(a));\n\n//# sourceURL=webpack://bulma-tagsfield/./docs/index.js?");
 
 /***/ }),
 
@@ -113,11 +24,19 @@ eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./docs
 /*!**************************!*\
   !*** ./src/tagsfield.js ***!
   \**************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _tagsfield_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tagsfield.scss */ \"./src/tagsfield.scss\");\n/* harmony import */ var _tagsfield_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_tagsfield_scss__WEBPACK_IMPORTED_MODULE_0__);\n\nconst tagFormat = /^(?!.*(__|--|_-|-_|\\s).*)[^\\W_][\\w\\-\\s]+[^\\W_]$/;\n\nclass Tagsfield {\n  constructor(el) {\n    this.el = el;\n    this.input = el.querySelector('input[type=\"hidden\"]');\n    this.editable = el.querySelector('span[contenteditable]');\n    el.addEventListener('click', () => this.editable.focus());\n    this.editable.addEventListener('focus', () => el.classList.add('is-focused'));\n    this.editable.addEventListener('blur', () => el.classList.remove('is-focused'));\n    this.editable.addEventListener('keydown', this.onKeyDown.bind(this));\n    this.editable.addEventListener('paste', event => {\n      event.preventDefault();\n      const text = event.clipboardData.getData('text/plain');\n      const tmp = document.createElement('div');\n      tmp.innerHTML = text;\n      document.execCommand('insertHTML', false, tmp.textContent.trim());\n    }); // Load tags from input.value\n\n    this.input.value.split(',').filter(v => v.length > 0).forEach(v => this.addTag(v));\n  }\n\n  validateTag(value, tagsValues) {\n    if (value.length > 2 && value.length <= 30 && tagsValues.indexOf(value) === -1 && tagFormat.test(value)) {\n      return true;\n    }\n  }\n\n  removeTag(tag) {\n    const values = this.input.value.split(',');\n    const index = Array.from(this.el.children).indexOf(tag);\n    values.splice(index, 1);\n    this.input.value = values.join(',');\n    this.el.removeChild(tag);\n  }\n\n  addTag(value) {\n    const tag = document.createElement('div');\n    tag.className = 'control';\n    tag.innerHTML = \"<div class=\\\"tags has-addons\\\">\\n      <span class=\\\"tag is-success\\\">\".concat(value, \"</span>\\n      <a class=\\\"tag is-delete\\\"></a>\\n    </div>\");\n    tag.querySelector('.is-delete').addEventListener('click', this.removeTag.bind(this, tag));\n    const inputs = this.el.children[this.el.children.length - 1];\n    this.el.insertBefore(tag, inputs);\n  }\n\n  onKeyDown(event) {\n    if (['Enter', ' ', ','].indexOf(event.key) >= 0) {\n      event.preventDefault();\n      const value = this.editable.textContent.trim();\n      const tagsValues = this.input.value.split(',').filter(v => v.length > 0);\n\n      if (!this.validateTag(value, tagsValues)) {\n        return;\n      }\n\n      tagsValues.push(value);\n      this.input.value = tagsValues.join(',');\n      this.addTag(value);\n      this.editable.innerHTML = '';\n    } else if (event.key === 'Backspace' && this.editable.textContent.length === 0 && this.el.children.length > 1) {\n      const index = this.el.children.length - 2;\n      const tag = this.el.children[index];\n      this.removeTag(tag);\n    }\n  }\n\n}\n\n/* harmony default export */ __webpack_exports__[\"default\"] = (Tagsfield);\n\n//# sourceURL=webpack:///./src/tagsfield.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _tagsfield_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./tagsfield.scss */ \"./src/tagsfield.scss\");\nconst tagFormat=/^(?!.*(__|--|_-|-_|\\s).*)[^\\W_][\\w\\-\\s]+[^\\W_]$/;class Tagsfield{constructor(a){// Load tags from input.value\nthis.el=a,this.input=a.querySelector(\"input[type=\\\"hidden\\\"]\"),this.editable=a.querySelector(\"span[contenteditable]\"),a.addEventListener(\"click\",()=>this.editable.focus()),this.editable.addEventListener(\"focus\",()=>a.classList.add(\"is-focused\")),this.editable.addEventListener(\"blur\",()=>a.classList.remove(\"is-focused\")),this.editable.addEventListener(\"keydown\",this.onKeyDown.bind(this)),this.editable.addEventListener(\"paste\",a=>{a.preventDefault();const b=a.clipboardData.getData(\"text/plain\"),c=document.createElement(\"div\");c.innerHTML=b,document.execCommand(\"insertHTML\",!1,c.textContent.trim())}),this.input.value.split(\",\").filter(a=>0<a.length).forEach(a=>this.addTag(a))}validateTag(a,b){if(2<a.length&&30>=a.length&&-1===b.indexOf(a)&&tagFormat.test(a))return!0}removeTag(a){const b=this.input.value.split(\",\"),c=Array.from(this.el.children).indexOf(a);b.splice(c,1),this.input.value=b.join(\",\"),this.el.removeChild(a)}addTag(a){const b=document.createElement(\"div\");b.className=\"control\",b.innerHTML=\"<div class=\\\"tags has-addons\\\">\\n      <span class=\\\"tag is-success\\\">\".concat(a,\"</span>\\n      <a class=\\\"tag is-delete\\\"></a>\\n    </div>\"),b.querySelector(\".is-delete\").addEventListener(\"click\",this.removeTag.bind(this,b));const c=this.el.children[this.el.children.length-1];this.el.insertBefore(b,c)}onKeyDown(a){if(0<=[\"Enter\",\" \",\",\"].indexOf(a.key)){a.preventDefault();const b=this.editable.textContent.trim(),c=this.input.value.split(\",\").filter(a=>0<a.length);if(!this.validateTag(b,c))return;c.push(b),this.input.value=c.join(\",\"),this.addTag(b),this.editable.innerHTML=\"\"}else if(\"Backspace\"===a.key&&0===this.editable.textContent.length&&1<this.el.children.length){const a=this.el.children.length-2,b=this.el.children[a];this.removeTag(b)}}}/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Tagsfield);\n\n//# sourceURL=webpack://bulma-tagsfield/./src/tagsfield.js?");
+
+/***/ }),
+
+/***/ "./docs/index.scss":
+/*!*************************!*\
+  !*** ./docs/index.scss ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://bulma-tagsfield/./docs/index.scss?");
 
 /***/ }),
 
@@ -125,11 +44,70 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _tag
 /*!****************************!*\
   !*** ./src/tagsfield.scss ***!
   \****************************/
-/*! no static exports found */
-/***/ (function(module, exports, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/tagsfield.scss?");
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://bulma-tagsfield/./src/tagsfield.scss?");
 
 /***/ })
 
-/******/ });
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		if(__webpack_module_cache__[moduleId]) {
+/******/ 			return __webpack_module_cache__[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	// startup
+/******/ 	// Load entry module
+/******/ 	__webpack_require__("./docs/index.js");
+/******/ 	// This entry module used 'exports' so it can't be inlined
+/******/ })()
+;
