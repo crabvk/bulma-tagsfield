bd7fafe1c101cb04731c7786bb517bd0066f5f93
Vyacheslav Konovalov <crabvk@protonmail.com>
2021-02-02 07:07:45 +0500

    Bulma 0.9.2, update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 module.exports = {
   mode: 'production',
   entry: { tagsfield: './src/tagsfield.js' },
   plugins: [
-    new MiniCssExtractPlugin(),
-    new OptimizeCssAssetsPlugin({
-      cssProcessorPluginOptions: {
-        preset: ['default', { discardComments: { removeAll: true } }]
-      }
-    })
+    new MiniCssExtractPlugin()
   ],
   module: {
     rules: [
@@ -35,5 +30,19 @@ module.exports = {
         ]
       }
     ]
-  }
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin({
+        minimizerOptions: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            }
+          ]
+        }
+      })
+    ]
+  },
 }
